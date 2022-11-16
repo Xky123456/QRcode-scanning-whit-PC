f79ceb5b62b4f76423b65a5d6254c415a68e32ce
jerry <Xukeyang1314@163.com>
2022-11-16 23:16:35 +0800

    创建了绝对路径扫描二维码和相对路径扫描二维码的.py文件 Create. py files of absolute path scanning QR code and relative path scanning QR code

---
+++ 内容/相对路径读取image图片.py
@@ -0,0 +1,68 @@
+import cv2
+import time
+from selenium import webdriver
+import webbrowser
+import sys
+from pyzbar.pyzbar import decode
+from webbrowser import Chrome
+from PIL import Image
+import zxing
+import os
+
+
+
+
+back = "y"
+
+if back == "y" or "Y":
+
+    name = input("选择文件名Select File Name")
+    file = input("选择图片格式Choose a picture format")
+
+    decocdeQR = decode(Image.open("image/" + name + "." + file))
+
+    url = decocdeQR[0].data.decode("ascii")
+    print(url)
+
+
+    #打开浏览器并搜索需要改进
+    webbrowser.open_new(url)
+    # print(webbrowser.get())
+
+    back = input("继续？continue?y/n")
+
+
+elif back == "n" or "N":
+    print("===================================================")
+    # print("弹窗将在1秒后关闭")
+    # time.sleep(1)
+    # break
+
+
+
+
+
+
+
+
+
+# houzhui = ["png","jpg","jpeg","bmp"]
+   # for i in houzhui:
+   #     if file == i:
+   #         val, _, _ = cv2.QRCodeDetector().detectAndDecode(cv2.imread("image/" + name + '.' + file))
+   #         print('QR码的内容是:',val)
+   #
+   #         sys.path.append("libs")
+   #         url = val
+   #         webbrowser.open(url)
+   #         print(webbrowser.get())
+   #
+   #         if val == " ":
+   #             print("NO HAVE")
+   #         else:
+   #             break
+   #
+   #         break
+   #     else:
+   #         print("不支持该格式")
+   #         break
---
+++ 内容/绝对路径读取.py
@@ -0,0 +1,21 @@
+import webbrowser
+from pyzbar.pyzbar import decode
+from PIL import Image
+
+
+
+while True:
+
+    # name = input("选择文件名")
+    file = input("请将图片拖入并按下回车")
+
+    decocdeQR = decode(Image.open(file))
+
+    url = decocdeQR[0].data.decode("ascii")
+    print(url)
+
+
+    #打开浏览器并搜索需要改进
+    webbrowser.open_new(url)
+
+    print("===================================================")
