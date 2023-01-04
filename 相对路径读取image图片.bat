1799d68c00175c5007f0e164603a9076fd3936cb
jerry <Xukeyang1314@163.com>
2023-01-04 11:04:19 +0800

    添加了bat批处理，即使你没有python37，你也可以使用

---
+++ 相对路径读取image图片.bat
@@ -0,0 +1,6 @@
+@echo off
+@chcp 65001
+@echo 开始运行
+call .\Python37\python.exe .\相对路径读取image图片.py
+@echo 处理完毕，请按任意键
+call pause
---
+++ 绝对路径读取.bat
@@ -0,0 +1,6 @@
+@echo off
+chcp 65001
+@echo 开始运行
+call .\Python37\python.exe .\picture.py
+@echo 处理完毕，请按任意键退出
+call pause
